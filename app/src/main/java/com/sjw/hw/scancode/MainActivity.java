5c6df3320042762373abfedd45d2a776d9a1a81e
sujiewei_git <925604487@qq.com>
2021-07-16 23:25:39 +0800

    init

---
+++ app/src/main/java/com/sjw/hw/scancode/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sjw.hw.scancode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
